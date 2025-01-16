10e702d7e14ea250db34327f2cacc173c09fd5ea
Abdelrahman Elsokary <abdalrahmanelsokarey@gmail.com>
2025-01-17 00:56:09 +0200

    Add: Dark Mode

---
+++ assets/scripts/main.js
@@ -0,0 +1,10 @@
+let them = document.getElementById('them');
+them.onclick = () => {
+  if (document.body.classList.contains('dark_mode')) {
+    document.body.classList.remove('dark_mode');
+    them.src = 'assets/icons/dark.webp';
+  } else {
+    document.body.classList.add('dark_mode');
+    them.src = 'assets/icons/light.webp';
+  }
+};
