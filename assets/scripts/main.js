840122bd81a9de8c8bd6ae56c4dbb4552c9db508
Abdelrahman Elsokary <abdalrahmanelsokarey@gmail.com>
2025-01-17 16:20:00 +0200

    Update: Font

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -23,7 +23,6 @@ fetch('https://www.myjsons.com/v/b7d03829')
       morningRemembrance += `
       <li>
       <p>${azkarData['أذكار الصباح'][i].content}<span>${azkarData['أذكار الصباح'][i].count}Time</span></p>
-        <br>
       <span>${azkarData['أذكار الصباح'][i].description}</span> 
       </li>
       `;
@@ -34,7 +33,6 @@ fetch('https://www.myjsons.com/v/b7d03829')
       eveningRemembrances += `
       <li>
       <p>${azkarData['أذكار المساء'][i].content}<span>${azkarData['أذكار المساء'][i].count}Time</span></p>
-        <br>
       <span>${azkarData['أذكار المساء'][i].description}</span>
       </li>
       `;
@@ -45,7 +43,6 @@ fetch('https://www.myjsons.com/v/b7d03829')
       obligatoryPrayers += `
       <li>
       <p>${azkarData['أذكار بعد السلام من الصلاة المفروضة'][i].content}<span>${azkarData['أذكار بعد السلام من الصلاة المفروضة'][i].count}Time</span></p>
-        <br>
       <span>${azkarData['أذكار بعد السلام من الصلاة المفروضة'][i].description}</span>
       </li>
       `;
@@ -56,7 +53,6 @@ fetch('https://www.myjsons.com/v/b7d03829')
       tasbih += `
       <li>
       <p>${azkarData['تسابيح'][i].content}<span>${azkarData['تسابيح'][i].count}Time</span></p> 
-      <br>
       <span>${azkarData['تسابيح'][i].description}</span>
       </li>
       `;
@@ -85,7 +81,6 @@ fetch('https://www.myjsons.com/v/b7d03829')
       quranicSupplications += `
       <li>
       <p>${azkarData['أدعية قرآنية'][i].content}<span>${azkarData['أدعية قرآنية'][i].count}Time</span></p>
-      <br>
       <span>${azkarData['أدعية قرآنية'][i].reference}</span> 
       </li>
       `;
@@ -97,7 +92,6 @@ fetch('https://www.myjsons.com/v/b7d03829')
       <li>
       <p>${azkarData['أدعية الأنبياء'][i].content}<span>${azkarData['أدعية الأنبياء'][i].count}Time</span>
       </p> 
-      <br>
       <span>${azkarData['أدعية الأنبياء'][i].description}</span>
       </li>
       `;
