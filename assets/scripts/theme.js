e908b6b249e8521319dee3e101a53e2a70891a86
Abdelrahman Elsokary <abdalrahmanelsokarey@gmail.com>
2025-01-22 23:19:47 +0200

    Creat: Theme File

--- assets/scripts/theme.js
+++ assets/scripts/theme.js
@@ -11,8 +11,3 @@ function toggelTheme() {
 them.onclick = () => {
   toggelTheme();
 };
-let dropDown = document.getElementById('drop_down');
-let toggelMenu = document.getElementById('menu');
-dropDown.onclick = () => {
-  toggelMenu.classList.toggle('drop_down');
-};
